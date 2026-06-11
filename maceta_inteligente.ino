325e9b94905a42b24084dddbfc3a9b173f180329
carolinashum1403-eng <carolinashum1403@gmail.com>
2026-06-11 01:03:02 -0600

    refactor: reemplazo de fotoresistor por BH1750FVI

--- maceta_inteligente.ino
+++ maceta_inteligente.ino
@@ -12,6 +12,8 @@
  */
 
 #include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+#include <BH1750.h>
 #include <WiFi.h>
 #include <WebServer.h>
 #include <ArduinoJson.h>
@@ -20,18 +22,15 @@ const char* SSID     = "TU_RED";
 const char* PASSWORD = "TU_PASSWORD";
 
 #define SOIL_PIN  34
-#define LIGHT_PIN 35
 #define NEOPIXEL_PIN   4
 #define NEOPIXEL_COUNT 10
-#define LIGHT_THRESHOLD 15
 
 Adafruit_NeoPixel strip(NEOPIXEL_COUNT, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 WebServer server(80);
 
 int soilRaw     = 0;
 int soilPercent = 0;
-int  lightRaw     = 0;
-bool lightDetected = false;
+float lightLux    = 0.0;
 
 void setup() {
   Serial.begin(115200);
@@ -39,6 +38,13 @@ void setup() {
   strip.begin();
   strip.setBrightness(80);
   strip.show();
+
+Wire.begin(21, 22);
+  if (!lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
+    Serial.println("Error: BH1750 no encontrado.");
+  } else {
+    Serial.println("BH1750 iniciado correctamente.");
+  }
 
   WiFi.begin(SSID, PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
@@ -54,19 +60,15 @@ void setup() {
 
 void loop() {
   server.handleClient();
-  readSoil();
+  readSensors();
   updateNeoPixel();
   delay(200);
 }
 
-void readSoil() {
-  soilRaw     = analogRead(SOIL_PIN);
-  soilPercent = map(soilRaw, 4095, 0, 0, 100);
-  soilPercent = constrain(soilPercent, 0, 100);
-  Serial.printf("Humedad: %d%% (raw: %d)\n", soilPercent, soilRaw);
-}
-
 void readSensors() {
+  if (lightMeter.measurementReady()) {
+    lightLux = lightMeter.readLightLevel();
+  }
   soilRaw      = analogRead(SOIL_PIN);
   soilPercent  = map(soilRaw, 4095, 0, 0, 100);
   soilPercent  = constrain(soilPercent, 0, 100);
@@ -74,17 +76,14 @@ void readSensors() {
   lightRaw      = analogRead(LIGHT_PIN);
   lightDetected = (lightRaw > LIGHT_THRESHOLD);
 
-  Serial.printf("Humedad: %d%% | Luz RAW: %d | Detectada: %s\n",
-                soilPercent, lightRaw, lightDetected ? "SI" : "NO");
+  Serial.printf("Humedad: %d%% | Luz: %.1f lux\n", soilPercent, lightLux);
 }
 
 void updateNeoPixel() {
-  if (!lightDetected) {
-    // Sin luz: todos azul
+  if (lightLux < 300.0) {
     for (int i = 0; i < NEOPIXEL_COUNT; i++)
-      strip.setPixelColor(i, strip.Color(0, 0, 200));
+      strip.setPixelColor(i, strip.Color(255, 30, 120));
   } else {
-    // Con luz: barra verde de humedad
     int ledsOn = map(soilPercent, 0, 100, 0, NEOPIXEL_COUNT);
     for (int i = 0; i < NEOPIXEL_COUNT; i++)
       strip.setPixelColor(i, i < ledsOn ? strip.Color(0, 200, 50) : strip.Color(0, 0, 0));
@@ -99,8 +98,7 @@ void handleRoot() {
 void handleData() {
   StaticJsonDocument<128> doc;
   doc["humedad_pct"] = soilPercent;
-  doc["luz_detectada"]  = lightDetected;
-  doc["luz_raw"]        = lightRaw;
+  doc["luz_lux"]     = (int)lightLux;
   String json;
   serializeJson(doc, json);
   server.sendHeader("Access-Control-Allow-Origin", "*");
